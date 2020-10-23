ae25a4d961f06826a1e04e7f3d9bfd27cae0eab3
Kuang-Yu Jeng <jengku@gmail.com>
2020-10-23 13:16:14 +0800

    use openmp instead of processes

--- hw1/train_all.sh
+++ hw1/train_all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-NUM_JOBS=8
 for ((i=1; i<=5; ++i)); do
-    echo "${1} model_init.txt data/train_seq_0${i}.txt model_0${i}.txt"
-done | xargs -P${NUM_JOBS} -n4 ./train
+    ./train ${1} model_init.txt data/train_seq_0${i}.txt model_0${i}.txt
+done
